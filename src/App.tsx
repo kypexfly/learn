0964c213bcdc26ff251bcd0d347ca08a9a917652
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-07-07 17:47:24 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "@/styles/globals.css"
+
+function App() {
+  return (
+    <div className="mb-2 mt-3 bg-blue-600 p-2 text-white dark:bg-gray-800">
+      Hola
+      <p>
+        fj dslkfdkls fdsjklf ds klk dfs
+        <span>d fsjklfdlkjs Fdslkfjdsl</span>
+      </p>
+    </div>
+  )
+}
+
+export default App
