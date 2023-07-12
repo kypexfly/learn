596e9504b06f1e19f710a7da1e590652fdcdfe88
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-07-12 10:15:50 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "@/styles/globals.css"
+
+function App() {
+  return (
+    <div className="m-10 rounded border bg-blue-100 p-10">
+      <h1 className="mb-3 text-3xl font-bold">Hello World</h1>
+      <p>
+        Boilerplate for React, TypeScript, Vite, ESLint, Prettier and
+        TailwindCSS.
+      </p>
+    </div>
+  )
+}
+
+export default App
