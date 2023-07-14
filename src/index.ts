8889541f7c65638ab27da05dfdb89b93b212732c
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-07-14 00:00:37 -0500

    deploy vercel

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,13 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`))
+
+app.get("/", (req, res) => {
+  res.send("Root: Hello World!")
+})
+
+app.get("/api", (req, res) => {
+  res.send("API: Hello World!")
+})
+
+module.exports = app
