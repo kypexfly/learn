05fb750fd426f17989498b9f650d0f5928530d7b
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-07-14 01:30:27 -0500

    husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "src/**/*.js": ["pnpm run add-build", "pnpm run add-build"],
+};
---
+++ build/index.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 4000;
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.get("/ping", (_req, res) => {
+    res.send("pong 🏓 2023");
+});
+app.get("/api", (_req, res) => {
+    res.json({
+        api: "v1",
+    });
+});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
