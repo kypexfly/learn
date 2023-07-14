0e0359c5389ea36424fbb34262ed6c32be905fb8
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-07-14 00:29:36 -0500

    up

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,16 @@ import express from "express"
 const app = express()
 const PORT = process.env.PORT || 4000
 
+app.get("/", (_req, res) => {
+  return res.send("Express Typescript on Vercel")
+})
+
+app.get("/ping", (_req, res) => {
+  return res.send("pong 🏓")
+})
+
+app.get("/api", (_req, res) => {
+  return res.send("API: Hello World!")
+})
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`))
-
-app.get("/", (req, res) => {
-  res.send("Root: Hello World!")
-})
-
-app.get("/api", (req, res) => {
-  res.send("API: Hello World!")
-})
