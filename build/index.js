74bf2f2f3d3813fcd37d70b4c30908c909d592f6
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-07-14 00:42:07 -0500

    update

--- build/index.js
+++ build/index.js
@@ -6,13 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 4000;
-app.get("/", (_req, res) => {
-    res.send("Express Typescript on Vercel");
-});
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
 app.get("/ping", (_req, res) => {
     res.send("pong 🏓");
 });
 app.get("/api", (_req, res) => {
-    res.send("API: Hello World!");
+    res.json({
+        api: "v1",
+    });
 });
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,17 @@ import express from "express"
 const app = express()
 const PORT = process.env.PORT || 4000
 
-app.get("/", (_req, res) => {
-  res.send("Express Typescript on Vercel")
-})
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/ping", (_req, res) => {
-  res.send("pong 🏓")
+  res.send("pong 🏓 2023")
 })
 
 app.get("/api", (_req, res) => {
-  res.send("API: Hello World!")
+  res.json({
+    api: "v1",
+  })
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`))
