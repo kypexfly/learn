414215f08cc5c5cfe8b646412988bd0c8e4a4cef
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-07-20 16:47:47 -0500

    vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+/// <reference types="vitest" />
+
+import path from "path"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./tests/setup.ts"],
+    testMatch: ["./tests/**/*.test.tsx"],
+    globals: true,
   },
 })
