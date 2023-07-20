502335c1341106eecb72642f7f5e2c79ab2b958a
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-07-20 10:06:26 -0500

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,10 @@
 import "@/styles/globals.css"
-import { useState } from "react"
-
-const Accordion = () => {
-  return (
-    <div className="w-96 border border-zinc-200">
-      <AccordionItem />
-      <AccordionItem />
-      <AccordionItem />
-    </div>
-  )
-}
-
-type AccordionItemProps = {
-  children?: React.ReactNode
-}
-
-const AccordionItem = ({ children }: AccordionItemProps) => {
-  const [active, setActive] = useState<boolean>(false)
-  return (
-    <div>
-      {children}
-      <div
-        className="flex cursor-pointer justify-between bg-blue-300 px-4 py-2 font-bold"
-        onClick={() => setActive(!active)}
-      >
-        Header <span>{active ? "-" : "+"}</span>
-      </div>
-      <div className={`${active ? "block" : "hidden"} px-4 py-2`}>Content</div>
-    </div>
-  )
-}
+import AccordionExample from "./examples/AccordionExample"
 
 function App() {
   return (
     <div className="App">
-      <Accordion />
+      <AccordionExample />
     </div>
   )
 }
