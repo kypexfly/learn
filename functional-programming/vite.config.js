7f27017a1c51e02c8388a8b75c0f97d5989a5298
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-08-06 19:34:56 -0500

    update vite build to include annotations

--- functional-programming/vite.config.js
+++ functional-programming/vite.config.js
@@ -3,11 +3,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    minify: "es",
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "index",
       fileName: "index",
+      formats: ["es"],
     },
     outDir: join(__dirname, "build"),
+  },
+  esbuild: {
+    minify: false,
+    keepNames: true,
   },
 });
