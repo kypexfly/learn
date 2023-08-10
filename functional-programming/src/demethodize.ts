d6ef1df1b88f4fb3b236c10438da03aa94711268
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-08-10 09:22:52 -0500

    add info of apply, call and bind in demethodize

--- functional-programming/src/demethodize.ts
+++ functional-programming/src/demethodize.ts
@@ -1,3 +1,8 @@
+// .apply(), .call() and .bind() change the context of `this` object
+// .apply(arg0, args): array of args
+// .call(arg0, ...args): positional args
+// .bind(arg0): returns a function with the defined context
+
 const demethodize1 =
   <T extends (arg0: any, ...args: any[]) => any>(fn: T) =>
   (arg0: any, ...args: Parameters<T>) =>
@@ -9,9 +14,9 @@ const demethodize2 =
     fn.call(arg0, ...args);
 
 const demethodize3 =
-  <T extends (arg0: any, ...args: any[]) => any>(fn: T) =>
-  (arg0: any, ...args: Parameters<T>): ReturnType<T> =>
-    fn.bind(arg0, ...args)();
+  <T extends (this: any, ...args: any[]) => any>(fn: T) =>
+  (thisArg: any, ...args: Parameters<T>): ReturnType<T> =>
+    fn.bind(thisArg)(...args);
 
 // Example
 
@@ -19,3 +24,7 @@ const string = "FUNCTIONAL";
 const map = demethodize1(Array.prototype.map);
 const toUpperCase = demethodize2(String.prototype.toUpperCase);
 const result = map(string, toUpperCase);
+
+const map2 = demethodize3(Array.prototype.map);
+const result2 = map2(string, toUpperCase);
+console.log(result2);
